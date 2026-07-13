e2fafa42979e39ae9ebc1d3ac26f0f5b92ca855c
chrcra2226 <chrcra2226@students.ecpi.edu>
2026-07-13 19:34:23 -0400

    File Path Changes
    Changed the file paths for Cassandra so it will run alongside Redis and MongoDB in the same folder.

--- Project/menu.py
+++ Project/menu.py
@@ -401,11 +401,6 @@ def run_mongodb_menu(data_filepath="data/Sample_Repos.json"):
 
         else:
             print("[WARN] Invalid choice. Try again.")
-# =======================================================
-# CASSANDRA Menu
-# =======================================================
-def run_cassandra_menu():
-    menu()
 
 # =====================================================
 # MAIN APPLICATION ENTRY POINT
@@ -442,281 +437,285 @@ def run_app():
         else:
             print("  [WARN] Invalid choice. Please try again.")
 
-# =====================================================
-# CONNECT TO CASSANDRA
-# =====================================================
+# =======================================================
+# CASSANDRA Menu
+# =======================================================
+def run_cassandra_menu():
+    # =====================================================
+    # CONNECT TO CASSANDRA
+    # =====================================================
 
-print("Connecting to local Cassandra database...")
+    print("Connecting to local Cassandra database...")
 
-cluster = Cluster()
-session = cluster.connect()
+    cluster = Cluster()
+    session = cluster.connect()
 
-# =====================================================
-# DATABASE SETUP
-# =====================================================
-
-session.execute("""
-CREATE KEYSPACE IF NOT EXISTS github_license_db
-WITH replication = {
-    'class':'SimpleStrategy',
-    'replication_factor':1
-}
-""")
-
-session.set_keyspace("github_license_db")
-
-session.execute("""
-CREATE TABLE IF NOT EXISTS repositories (
-    repo_name TEXT PRIMARY KEY,
-    license TEXT
-)
-""")
-
-print("Database setup complete.")
-
-# =====================================================
-# Load data from Licenses.json file into Cassandra
-# =====================================================
-with open("SDC435L-main/Project/data/Licenses.json", "r") as f:
-    for line in f:
-        record = json.loads(line.strip())
-        session.execute("""
-            INSERT INTO repositories (repo_name, license)
-            VALUES (%s, %s)
-        """, (record["repo_name"], record["license"]))
-print("Licenses data loaded into Cassandra.")
-
-# =====================================================
-# CRUD FUNCTIONS
-# =====================================================
-
-def create_repository():
-
-    print("\n--- Create Repository ---")
-
-    repo_name = input("Repository Name: ")
-    license_name = input("License: ")
+    # =====================================================
+    # DATABASE SETUP
+    # =====================================================
 
     session.execute("""
-    INSERT INTO repositories (repo_name, license)
-    VALUES (%s, %s)
-    """, (repo_name, license_name))
-
-    print("Repository created successfully.")
-
-
-def read_repository():
-
-    print("\n--- Read Repository ---")
-
-    repo_name = input("Repository Name: ")
-
-    rows = session.execute("""
-    SELECT *
-    FROM repositories
-    WHERE repo_name=%s
-    """, [repo_name])
-
-    found = False
-
-    for row in rows:
-        found = True
-
-        print("\nRepository Found")
-        print(f"Repository : {row.repo_name}")
-        print(f"License    : {row.license}")
-
-    if not found:
-        print("Repository not found.")
-
-
-def update_repository():
-
-    print("\n--- Update Repository ---")
-
-    repo_name = input("Repository Name: ")
-    new_license = input("New License: ")
-
-    session.execute("""
-    UPDATE repositories
-    SET license=%s
-    WHERE repo_name=%s
-    """, (new_license, repo_name))
-
-    print("Repository updated successfully.")
-
-
-def delete_repository():
-
-    print("\n--- Delete Repository ---")
-
-    repo_name = input("Repository Name: ")
-
-    session.execute("""
-    DELETE FROM repositories
-    WHERE repo_name=%s
-    """, [repo_name])
-
-    print("Repository deleted successfully.")
-
-
-# =====================================================
-# ANALYTICS FUNCTIONS
-# =====================================================
-
-def license_distribution():
-
-    rows = session.execute("""
-    SELECT license
-    FROM repositories
+    CREATE KEYSPACE IF NOT EXISTS github_license_db
+    WITH replication = {
+        'class':'SimpleStrategy',
+        'replication_factor':1
+    }
     """)
 
-    licenses = [row.license for row in rows]
+    session.set_keyspace("github_license_db")
 
-    counts = Counter(licenses)
-
-    print("\nTop License Distribution\n")
-
-    for license_name, count in counts.most_common(10):
-        print(f"{license_name:<20} {count}")
-
-#FUNCTION 1
-#=======================================
-def search_by_license():
-
-    target_license = input(
-        "\nEnter License Name: "
+    session.execute("""
+    CREATE TABLE IF NOT EXISTS repositories (
+        repo_name TEXT PRIMARY KEY,
+        license TEXT
     )
-
-    rows = session.execute("""
-    SELECT *
-    FROM repositories
     """)
 
-    total = 0
+    print("Database setup complete.")
 
-    print("\nRepositories\n")
+    # =====================================================
+    # Load data from Licenses.json file into Cassandra
+    # =====================================================
+    with open("data/Licenses.json", "r") as f:
+        for line in f:
+            record = json.loads(line.strip())
+            session.execute("""
+                INSERT INTO repositories (repo_name, license)
+                VALUES (%s, %s)
+            """, (record["repo_name"], record["license"]))
+    print("Licenses data loaded into Cassandra.")
+    
+    # =====================================================
+    # CRUD FUNCTIONS
+    # =====================================================
 
-    for row in rows:
+    def create_repository():
 
-        if row.license == target_license:
+        print("\n--- Create Repository ---")
 
-            print(row.repo_name)
-            total += 1
+        repo_name = input("Repository Name: ")
+        license_name = input("License: ")
 
-    print(f"\nTotal Found: {total}")
+        session.execute("""
+        INSERT INTO repositories (repo_name, license)
+        VALUES (%s, %s)
+        """, (repo_name, license_name))
 
-#FUNCTION 2
-#================================================
-def visualize_licenses():
-
-    rows = session.execute("""
-    SELECT license
-    FROM repositories
-    """)
-
-    licenses = [row.license for row in rows]
-
-    counts = Counter(licenses)
-
-    top_ten = counts.most_common(10)
-
-    labels = [item[0] for item in top_ten]
-    values = [item[1] for item in top_ten]
-
-    plt.figure(figsize=(10, 6))
-    plt.bar(labels, values)
-
-    plt.title("Top 10 GitHub Licenses")
-    plt.xlabel("License Type")
-    plt.ylabel("Number of Repositories")
-
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-
-    plt.show()
-
-#fUNTION 3
-#===================================================
-def record_count():
-
-    rows = session.execute("""
-    SELECT repo_name
-    FROM repositories
-    """)
-
-    count = len(list(rows))
-
-    print(f"\nTotal Records: {count}")
+        print("Repository created successfully.")
 
 
-# =====================================================
-# MENU
-# =====================================================
+    def read_repository():
 
-def menu():
+        print("\n--- Read Repository ---")
 
-    while True:
+        repo_name = input("Repository Name: ")
 
-        print("\n" + "=" * 60)
-        print("      GitHub License Analytics System")
-        print("=" * 60)
+        rows = session.execute("""
+        SELECT *
+        FROM repositories
+        WHERE repo_name=%s
+        """, [repo_name])
 
-        print("\nCRUD OPERATIONS")
-        print("  1. Create Repository")
-        print("  2. Read Repository")
-        print("  3. Update Repository")
-        print("  4. Delete Repository")
+        found = False
 
-        print("\nANALYTICS")
-        print("  5. License Distribution")
-        print("  6. Search by License")
-        print("  7. Visualize Top Licenses")
+        for row in rows:
+            found = True
 
-        print("\nSYSTEM")
-        print("  8. Show Record Count")
-        print("  9. Exit")
+            print("\nRepository Found")
+            print(f"Repository : {row.repo_name}")
+            print(f"License    : {row.license}")
 
-        print("-" * 60)
+        if not found:
+            print("Repository not found.")
 
-        choice = input("Select an option (1-9): ")
 
-        if choice == "1":
-            create_repository()
+    def update_repository():
 
-        elif choice == "2":
-            read_repository()
+        print("\n--- Update Repository ---")
 
-        elif choice == "3":
-            update_repository()
+        repo_name = input("Repository Name: ")
+        new_license = input("New License: ")
 
-        elif choice == "4":
-            delete_repository()
+        session.execute("""
+        UPDATE repositories
+        SET license=%s
+        WHERE repo_name=%s
+        """, (new_license, repo_name))
 
-        elif choice == "5":
-            license_distribution()
+        print("Repository updated successfully.")
 
-        elif choice == "6":
-            search_by_license()
 
-        elif choice == "7":
-            visualize_licenses()
+    def delete_repository():
 
-        elif choice == "8":
-            record_count()
+        print("\n--- Delete Repository ---")
 
-        elif choice == "9":
+        repo_name = input("Repository Name: ")
 
-            print("\nThank you for using")
-            print("GitHub License Analytics System")
-            print("Goodbye!")
+        session.execute("""
+        DELETE FROM repositories
+        WHERE repo_name=%s
+        """, [repo_name])
 
-            break
+        print("Repository deleted successfully.")
 
-        else:
 
-            print("\nInvalid selection. Try again.")
+    # =====================================================
+    # ANALYTICS FUNCTIONS
+    # =====================================================
 
+    def license_distribution():
+
+        rows = session.execute("""
+        SELECT license
+        FROM repositories
+        """)
+
+        licenses = [row.license for row in rows]
+
+        counts = Counter(licenses)
+
+        print("\nTop License Distribution\n")
+
+        for license_name, count in counts.most_common(10):
+            print(f"{license_name:<20} {count}")
+
+    #FUNCTION 1
+    #=======================================
+    def search_by_license():
+
+        target_license = input(
+            "\nEnter License Name: "
+        )
+
+        rows = session.execute("""
+        SELECT *
+        FROM repositories
+        """)
+
+        total = 0
+
+        print("\nRepositories\n")
+
+        for row in rows:
+
+            if row.license == target_license:
+
+                print(row.repo_name)
+                total += 1
+
+        print(f"\nTotal Found: {total}")
+
+    #FUNCTION 2
+    #================================================
+    def visualize_licenses():
+
+        rows = session.execute("""
+        SELECT license
+        FROM repositories
+        """)
+
+        licenses = [row.license for row in rows]
+
+        counts = Counter(licenses)
+
+        top_ten = counts.most_common(10)
+
+        labels = [item[0] for item in top_ten]
+        values = [item[1] for item in top_ten]
+
+        plt.figure(figsize=(10, 6))
+        plt.bar(labels, values)
+
+        plt.title("Top 10 GitHub Licenses")
+        plt.xlabel("License Type")
+        plt.ylabel("Number of Repositories")
+
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.show()
+
+    #fUNTION 3
+    #===================================================
+    def record_count():
+
+        rows = session.execute("""
+        SELECT repo_name
+        FROM repositories
+        """)
+
+        count = len(list(rows))
+
+        print(f"\nTotal Records: {count}")
+
+
+    # =====================================================
+    # MENU
+    # =====================================================
+
+    def menu():
+
+        while True:
+
+            print("\n" + "=" * 60)
+            print("      GitHub License Analytics System")
+            print("=" * 60)
+
+            print("\nCRUD OPERATIONS")
+            print("  1. Create Repository")
+            print("  2. Read Repository")
+            print("  3. Update Repository")
+            print("  4. Delete Repository")
+
+            print("\nANALYTICS")
+            print("  5. License Distribution")
+            print("  6. Search by License")
+            print("  7. Visualize Top Licenses")
+
+            print("\nSYSTEM")
+            print("  8. Show Record Count")
+            print("  9. Exit")
+
+            print("-" * 60)
+
+            choice = input("Select an option (1-9): ")
+
+            if choice == "1":
+                create_repository()
+
+            elif choice == "2":
+                read_repository()
+
+            elif choice == "3":
+                update_repository()
+
+            elif choice == "4":
+                delete_repository()
+
+            elif choice == "5":
+                license_distribution()
+
+            elif choice == "6":
+                search_by_license()
+
+            elif choice == "7":
+                visualize_licenses()
+
+            elif choice == "8":
+                record_count()
+
+            elif choice == "9":
+
+                print("\nThank you for using")
+                print("GitHub License Analytics System")
+                print("Goodbye!")
+
+                break
+
+            else:
+
+                print("\nInvalid selection. Try again.")
+    menu()
           
 # ─────────────────────────────────────────
 #  ENTRY POINT
